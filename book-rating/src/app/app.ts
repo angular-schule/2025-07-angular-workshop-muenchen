74842362dabb3fb4a85e124737aa5c022fbe8d8c
Ferdinand Malcher <ferdinand@malcher.media>
2025-07-28 14:08:22 +0200

    Dashboard einbinden

--- book-rating/src/app/app.ts
+++ book-rating/src/app/app.ts
@@ -1,13 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterDisplay } from "./counter-display/counter-display";
+import { DashboardPage } from "./books/dashboard-page/dashboard-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterDisplay],
+  imports: [RouterOutlet, CounterDisplay, DashboardPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('book-rating');
 }
+
+
+/*
+TODO:
+- Datenstruktur
+- Array mit Büchern
+- Daten speichern in Signal
+- Template zur Anzeige
+- (Buchkomponente?)
+
+*/
+
