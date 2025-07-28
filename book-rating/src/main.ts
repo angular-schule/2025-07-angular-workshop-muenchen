2f083146eb06f5f22886b2990abf7413e2f308f0
Ferdinand Malcher <ferdinand@malcher.media>
2025-07-28 11:11:43 +0200

    TypeScript

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,3 +4,80 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
+
+
+//////////////////////////////////
+
+
+
+export class Customer {
+  // Property
+  #name?: string;
+
+  protected role = 'admin';
+
+  #id = 5;
+
+  constructor() {
+    this.#name = 'Ferdinand';
+    this.#fooBar(5);
+  }
+
+  #fooBar(arg: number): string {
+    this.#name = 'dfhdfjk';
+    setTimeout(() => {
+      console.log(this.#name);
+    }, 2000);
+    return '';
+  }
+}
+
+
+export class UnprivilegedCustomer extends Customer {
+  constructor() {
+    super();
+    this.role = 'user'; // möglich weil protected
+    // this.#name = ''; // nicht möglich weil private
+  }
+}
+
+
+export const myCustomer = new Customer();
+// myCustomer.#name = 'dfdsf'; // geht nicht
+// myCustomer.id = 6
+// myCustomer.role = 6
+
+const foo = 5;
+// foo = 6;
+
+
+//////////////////////////////////
+
+
+/*const abc = function (arg) {
+  return arg + 1;
+}
+
+
+
+const abc1 = (arg: number) => true;*/
+
+
+
+
+
+//////////////////////////////////
+
+export function bar(arg: string | number) {
+  if (typeof arg === 'number') {
+    return arg + 1;
+  } else {
+    return arg;
+  }
+}
+
+bar(5)
+bar('')
+// bar(true) // geht nicht
